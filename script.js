ab0009b5fbf1366645c9799b634d9c82c6835fc2
Abbush <glgberat@yahoo.com>
2022-01-07 23:46:53 -0500

    Series of Window Prompts

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // Assignment code here
-
+function generatePassword() {
+  var criteria = {
+    length:8,
+    lowercase: new Boolean(true),
+    uppercase: new Boolean(true),
+    numeric: new Boolean(true),
+    special: new Boolean(true),
+  };
+criteria.length= parseInt (window.prompt("Please enter the length of your password? (min:8 Max:128)"), 10);
+criteria.lowercase= window.confirm("Would you like to have lowercase in your password?");
+criteria.uppercase= window.confirm("Would you like to have UPPERCASE in your password?");
+criteria.numeric= window.confirm("Would you like to have NUMERIC(0-9) in your password?");
+criteria.special= window.confirm("Would you like to have SPECIAL CHARACTERS ($%#&) in your password?");
+console.log(criteria);
+  return;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
