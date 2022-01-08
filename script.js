787ba10d350dd32f842ab4373078e6b215229b26
Abbush <glgberat@yahoo.com>
2022-01-08 06:38:13 -0500

    Selected criteria at least 1 character validated

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var password="";
 
 
 var generatePassword = function() {
+var GuaranteeAdded =0;
 var PoolofChars ="";
 var criteria = {
     length:8,
@@ -23,22 +24,39 @@ if (!(isNaN(criteria.length) || criteria.length<8 || criteria.length>128)) {
 criteria.lowercase= window.confirm("Would you like to have lowercase in your password?");
   if (criteria.lowercase){
     PoolofChars += charactersLower;
+    password+=charactersLower.charAt(Math.floor(Math.random() * charactersLower.length+1));
+    GuaranteeAdded++;
+    console.log(GuaranteeAdded);
+console.log(password);
    }
 
 
 criteria.uppercase= window.confirm("Would you like to have UPPERCASE in your password?");
 if(criteria.uppercase) {
 PoolofChars += charactersUpper;
+password+=charactersUpper.charAt(Math.floor(Math.random() * charactersUpper.length+1));
+GuaranteeAdded++;
+console.log(GuaranteeAdded);
+console.log(password);
   }
 criteria.numeric= window.confirm("Would you like to have NUMERIC(0-9) in your password?");
 if(criteria.numeric) {
 PoolofChars += charactersNumeric;
+password+=charactersNumeric.charAt(Math.floor(Math.random() * charactersNumeric.length+1));
+    GuaranteeAdded++;
+    console.log(GuaranteeAdded);
+console.log(password);
 
  }
 
 criteria.special= window.confirm("Would you like to have SPECIAL CHARACTERS (i.e $%#&...) in your password?");
 if(criteria.special) {
   PoolofChars += charactersSpecial;
+  password+=charactersSpecial.charAt(Math.floor(Math.random() * charactersSpecial.length+1));
+    GuaranteeAdded++;
+
+    console.log(GuaranteeAdded);
+console.log(password);
   
    }
 
@@ -55,7 +73,7 @@ generatePassword();
 }
 
 console.log(PoolofChars);
-for(i=0; i<criteria.length; i++) 
+for(i=0; i<criteria.length-GuaranteeAdded; i++) 
 {
   const charactersLength = PoolofChars.length;
   password += PoolofChars.charAt(Math.floor(Math.random() * charactersLength+1));
@@ -63,6 +81,8 @@ for(i=0; i<criteria.length; i++)
 
 }
 console.log(password);
+deneme.innerHTML = "Your New Password";
+
 return password;
 }
 
@@ -73,6 +93,7 @@ return password;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var deneme = document.querySelector("#berat");
 
 // Write password to the #password input
 function writePassword() {
