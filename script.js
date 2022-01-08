08ba22e6620891cb7738ae994bf824b338f10530
Abbush <glgberat@yahoo.com>
2022-01-08 04:00:41 -0500

    Creating Pool of Characters to Generate Pswd

--- script.js
+++ script.js
@@ -1,29 +1,81 @@
 // Assignment code here
-function generatePassword() {
-  var criteria = {
+const charactersLower = 'abcdefghijklmnopqrstuvwxyz';
+const charactersUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const charactersNumeric = '0123456789';
+const charactersSpecial= '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+
+
+
+var generatePassword = function() {
+  
+var PoolofChars = ' ';
+var criteria = {
     length:8,
     lowercase: new Boolean(true),
     uppercase: new Boolean(true),
     numeric: new Boolean(true),
     special: new Boolean(true),
   };
+ 
 criteria.length= parseInt (window.prompt("Please enter the length of your password? (min:8 Max:128)"), 10);
 
 
 if (!(isNaN(criteria.length) || criteria.length<8 || criteria.length>128)) {
 criteria.lowercase= window.confirm("Would you like to have lowercase in your password?");
+  if (criteria.lowercase){
+    PoolofChars += charactersLower;
+   }
+
+
 criteria.uppercase= window.confirm("Would you like to have UPPERCASE in your password?");
+if(criteria.uppercase) {
+PoolofChars += charactersUpper;
+  }
 criteria.numeric= window.confirm("Would you like to have NUMERIC(0-9) in your password?");
-criteria.special= window.confirm("Would you like to have SPECIAL CHARACTERS ($%#&) in your password?");
-console.log(criteria);
+if(criteria.numeric) {
+PoolofChars += charactersNumeric;
+
+ }
+
+criteria.special= window.confirm("Would you like to have SPECIAL CHARACTERS (i.e $%#&...) in your password?");
+if(criteria.special) {
+  PoolofChars += charactersSpecial;
+  
+   }
+
 }
 
 else {
   window.alert("Please Enter a valid input");
   generatePassword();
 }
+
+if (!((criteria.lowercase || criteria.uppercase) || (criteria.numeric || criteria.special))) {
+window.alert(" You MUST choose at LEAST 1 character type");
+generatePassword();
 }
+
+
+
+}
+
+
+/*for(i=0; i<criteria.length; i++) 
+{
+
+
+}
+
+}
+while()
+var PasswordCharPool= function() {
   
+  
+
+  return result; 
+ }
+
+  */
 
 
 
