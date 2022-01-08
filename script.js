5eb410ceedadda0f52098859446d33a066f74880
Abbush <glgberat@yahoo.com>
2022-01-08 00:34:59 -0500

    Validating correct input

--- script.js
+++ script.js
@@ -8,13 +8,24 @@ function generatePassword() {
     special: new Boolean(true),
   };
 criteria.length= parseInt (window.prompt("Please enter the length of your password? (min:8 Max:128)"), 10);
+
+
+if (!(isNaN(criteria.length) || criteria.length<8 || criteria.length>128)) {
 criteria.lowercase= window.confirm("Would you like to have lowercase in your password?");
 criteria.uppercase= window.confirm("Would you like to have UPPERCASE in your password?");
 criteria.numeric= window.confirm("Would you like to have NUMERIC(0-9) in your password?");
 criteria.special= window.confirm("Would you like to have SPECIAL CHARACTERS ($%#&) in your password?");
 console.log(criteria);
-  return;
 }
+
+else {
+  window.alert("Please Enter a valid input");
+  generatePassword();
+}
+}
+  
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
